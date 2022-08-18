8064549874f4f222c65abfb91fad82878a5c456e
Eclipsian <erica2007210@gmail.com>
2022-08-18 18:10:11 -0400

    Startup

---
+++ src/interfaces.ts
@@ -0,0 +1,52 @@
+export interface Champion {
+    blurb: string;
+    id: string;
+    image: {
+      full: string;
+      sprite: string;
+      group: string;
+    };
+    info: {
+      attack: number;
+      defense: number;
+      magic: number;
+      difficulty: number;
+    };
+    name: string;
+    partype: string;
+    key: string;
+    stats: {
+      armor: number;
+      armorperlevel: number;
+      attackdamage: number;
+      attackdamageperlevel: number;
+      attackrange: number;
+      attackspeed: number;
+      attackspeedperlevel: number;
+      crit: number;
+      critperlevel: number;
+      hp: number;
+      hpperlevel: number;
+      hpregen: number;
+      hpregenperlevel: number;
+      movespeed: number;
+      mp: number;
+      mpperlevel: number;
+      mpregen: number;
+      mpregenperlevel: number;
+      spellblock: number;
+      spellblockperlevel: number;
+    };
+    tags: ChampionTag[];
+    title: string;
+}
+  
+export enum ChampionTag {
+    Assassin = 'Assassin',
+    Fighter = 'Fighter',
+    Mage = 'Mage',
+    Marksman = 'Marksman',
+    Support = 'Support',
+    Tank = 'Tank',
+    All = 'All',
+}

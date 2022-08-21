87fc93c82413aeaa176b6ed531a5c4ac87905340
Eclipsian <erica2007210@gmail.com>
2022-08-20 23:26:40 -0400

    hw2

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -14,12 +14,13 @@ const HomePage = (props: Props) => {
     ChampionTag.All,
   );
   const classTag = [
-    { id: 1, name: 'Assassin', unavailable: false },
-    { id: 2, name: 'Fighter', unavailable: false },
-    { id: 3, name: 'Mage', unavailable: false },
-    { id: 4, name: 'Marksman', unavailable: true },
-    { id: 5, name: 'Support', unavailable: false },
-    { id: 6, name: 'Tank', unavailable: false },
+    { id: 1, name: 'All Tags' },
+    { id: 2, name: 'Assassin' },
+    { id: 3, name: 'Fighter' },
+    { id: 4, name: 'Mage' },
+    { id: 5, name: 'Marksman' },
+    { id: 6, name: 'Support' },
+    { id: 7, name: 'Tank' },
   ];
   const [selected, setSelected] = useState(classTag[0])
 
@@ -40,9 +41,9 @@ const HomePage = (props: Props) => {
   return (
     <div className='px-4'>
 
-      <form className='p-5 flex gap-5'>
+      <form className='flex gap-5 p-5'>
         <input
-          className='px-2 py-1 basis-3/4 shadow-md rounded-lg'
+          className='px-2 py-1 rounded-lg shadow-md basis-3/4'
           value={searchValue}
           onChange={(e) => setSearchValue(e.target.value)}
           type='search'
@@ -53,53 +54,46 @@ const HomePage = (props: Props) => {
 
 <div className="w-72 basis-1/4">
   <Listbox value={selected} onChange={setSelected}>
-    <div className="relative mt-1">
-      <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+    <div className="relative">
+      <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md sm:text-sm">
         <span className="block truncate">{selected.name}</span>
-        <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+        <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
           <SelectorIcon
-            className="h-5 w-5 text-gray-400"
+            className="w-5 h-5 text-gray-400"
             aria-hidden="true"
           />
         </span>
       </Listbox.Button>
-      <Transition
-        as={Fragment}
-        leave="transition ease-in duration-100"
-        leaveFrom="opacity-100"
-        leaveTo="opacity-0"
-      >
-        <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-          {classTag.map((classTag, classTagIdx) => (
-            <Listbox.Option
-              key={classTagIdx}
-              className={({ active }) =>
-                `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                  active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
-                }`
-              }
-              value={classTag}
-            >
-              {({ selected }) => (
-                <>
-                  <span
-                    className={`block truncate ${
-                      selected ? 'font-medium' : 'font-normal'
-                    }`}
-                  >
-                    {classTag.name}
+      <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 focus:outline-none sm:text-sm">
+        {classTag.map((classTag, classTagIdx) => (
+          <Listbox.Option
+            key={classTagIdx}
+            className={({ active }) =>
+              `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+              }`
+            }
+            value={classTag}
+          >
+            {({ selected }) => (
+              <>
+                <span
+                  className={`block truncate ${
+                    selected ? 'font-medium' : 'font-normal'
+                  }`}
+                >
+                  {classTag.name}
+                </span>
+                {selected ? (
+                  <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                    <CheckIcon className="w-5 h-5" aria-hidden="true" />
                   </span>
-                  {selected ? (
-                    <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
-                      <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                    </span>
-                  ) : null}
-                </>
-              )}
-            </Listbox.Option>
-          ))}
-        </Listbox.Options>
-      </Transition>
+                ) : null}
+              </>
+            )}
+          </Listbox.Option>
+        ))}
+      </Listbox.Options>
     </div>
   </Listbox>
 </div>
@@ -149,13 +143,13 @@ const HomePage = (props: Props) => {
 
                 <div className='space-x-2'>
                   {champion.tags.map((tag) => (
-                    <span className='inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-yellow-100 text-yellow-800'>
+                    <span className='inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-amber-100 text-amber-800'>
                       {tag}
                     </span>
                   ))}
                 </div>
 
-                <h1 className='underline text-sm'>Stats ( / per level)</h1>
+                <h1 className='text-sm underline'>Stats ( / per level)</h1>
                 <div className='text-xs'>
                   <p> Armor : {champion.stats.armor} / {champion.stats.armorperlevel} </p>
                   <p> Attack Damage : {champion.stats.attackdamage} / {champion.stats.attackdamageperlevel} </p>

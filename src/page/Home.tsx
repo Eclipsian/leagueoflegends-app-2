88a6e904a7efeec82f0927ca64c4f31737247a01
Eclipsian <erica2007210@gmail.com>
2022-08-23 01:48:44 -0400

    cw3-1

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState, Fragment } from 'react';
+import React, { useEffect } from 'react';
 import '../App.css';
 import axios from 'axios';
 import { Champion, ChampionTag } from '../interfaces';
-import { Listbox } from '@headlessui/react'
-import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
+import Dropdown from '../components/Dropdown';
+import { MailIcon, SearchIcon } from '@heroicons/react/solid';
 
 type Props = {};
 
@@ -13,17 +13,8 @@ const HomePage = (props: Props) => {
   const [selectedTag, setSelectedTag] = React.useState<ChampionTag>(
     ChampionTag.All,
   );
-  const classTag = [
-    { id: 1, name: 'All Tags' },
-    { id: 2, name: 'Assassin' },
-    { id: 3, name: 'Fighter' },
-    { id: 4, name: 'Mage' },
-    { id: 5, name: 'Marksman' },
-    { id: 6, name: 'Support' },
-    { id: 7, name: 'Tank' },
-  ];
-  const [selected, setSelected] = useState(classTag[0])
 
+  
   useEffect(() => {
     axios
       .get(
@@ -41,74 +32,32 @@ const HomePage = (props: Props) => {
   return (
     <div className='px-4'>
 
-      <form className='flex gap-5 p-5'>
-        <input
-          className='px-2 py-1 rounded-lg shadow-md basis-3/4'
-          value={searchValue}
-          onChange={(e) => setSearchValue(e.target.value)}
-          type='search'
-          placeholder='Search by name'
-        /> 
-
-
-
-<div className="w-72 basis-1/4">
-  <Listbox value={selected} onChange={setSelected}>
-    <div className="relative">
-      <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md sm:text-sm">
-        <span className="block truncate">{selected.name}</span>
-        <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-          <SelectorIcon className="w-5 h-5 text-gray-400"/>
-        </span>
-      </Listbox.Button>
-      <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 sm:text-sm">
-        {classTag.map((classTag, classTagIdx) => (
-          <Listbox.Option
-            key={classTagIdx}
-            className={({ active }) =>
-              `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
-              }`
-            }
-            value={classTag}
+      <form className='grid grid-cols-2 gap-5 p-5 md:grid-cols-3 lg:grid-cols-4'>
+        <div>
+          <label
+            htmlFor='name'
+            className='block text-sm font-medium text-gray-700'
           >
-            {({ selected }) => (
-              <>
-                <span
-                  className={`block truncate ${
-                    selected ? 'font-medium' : 'font-normal'
-                  }`}
-                >
-                  {classTag.name}
-                </span>
-                {selected ? (
-                  <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
-                    <CheckIcon className="w-5 h-5" aria-hidden="true" />
-                  </span>
-                ) : null}
-              </>
-            )}
-          </Listbox.Option>
-        ))}
-      </Listbox.Options>
-    </div>
-  </Listbox>
-</div>
-
-
-
-        {/* <select
-        value={selectedTag as string}
-        onChange={(e) => setSelectedTag(e.target.value as ChampionTag)}>
-          <option value={ChampionTag.All}>Select a champion tag</option>
-          {Object.values(ChampionTag).map((tag) => (
-            <option key={tag} value={tag}>
-              {tag}
-            </option>
-          ))}
-        </select> */}
-
-
+            Search by name
+          </label>
+          <div className='relative mt-1 rounded-md shadow-sm'>
+            <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
+              <SearchIcon className='w-5 h-5 text-gray-400' aria-hidden='true' />
+            </div>
+            <input
+              type='search'
+              name='name'
+              id='name'
+              className='block w-full py-2.5 pr-2 shadow-md pl-10 border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+              placeholder='Name'
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+            />
+          </div>
+        </div>
+        
+        <Dropdown setSelectedTag={setSelectedTag} selectedTag={selectedTag} />
+        
       </form>
 
       <div className='grid grid-cols-3 gap-4 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8'>
